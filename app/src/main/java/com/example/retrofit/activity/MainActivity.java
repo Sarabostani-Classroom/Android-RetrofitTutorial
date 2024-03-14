a8fc9ab91ff7e23567bfd72fc207b4b995103e1d
Houman Sarabostani <sarabostani@outlook.com>
2024-03-13 18:39:43 -0700

    Project upgraded

--- app/src/main/java/com/example/retrofit/activity/MainActivity.java
+++ app/src/main/java/com/example/retrofit/activity/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.retrofit.activity;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +10,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.retrofit.R;
 import com.example.retrofit.adapter.CustomAdapter;
+import com.example.retrofit.model.Comment;
 import com.example.retrofit.model.RetroPhoto;
 import com.example.retrofit.network.GetDataService;
-import com.example.retrofit.network.RetrofitClientInstance;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.internal.EverythingIsNonNull;
 
 
@@ -26,45 +28,63 @@ public class MainActivity extends AppCompatActivity {
 
     private CustomAdapter adapter;
     private RecyclerView recyclerView;
-    ProgressDialog progressDoalog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        progressDoalog = new ProgressDialog(MainActivity.this);
-        progressDoalog.setMessage("Loading....");
-        progressDoalog.show();
+
+        final String BASE_URL = "https://jsonplaceholder.typicode.com";
+
+        // Volley
+        // Singleton pattern
+        Retrofit retrofit = new retrofit2.Retrofit.Builder()
+                        .baseUrl(BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
 
         // Create a reference for the RetrofitInstance interface
-        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
+        GetDataService service = retrofit.create(GetDataService.class);
 
         Call<List<RetroPhoto>> call = service.getAllPhotos();
+
+
         call.enqueue(new Callback<List<RetroPhoto>>() {
 
             @Override
             @EverythingIsNonNull
             public void onResponse(Call<List<RetroPhoto>> call, Response<List<RetroPhoto>> response) {
-                progressDoalog.dismiss();
                 generateDataList(response.body());
             }
 
             @Override
             @EverythingIsNonNull
             public void onFailure(Call<List<RetroPhoto>> call, Throwable t) {
-                progressDoalog.dismiss();
                 Toast.makeText(MainActivity.this,
                         "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
             }
         });
+
+        Call<List<Comment>> commentCall = service.getAllComments();
+        commentCall.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+                Log.i("Comment", response.body().get(0).body);
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
+
+            }
+        });
+
     }
 
     private void generateDataList(List<RetroPhoto> photoList) {
         recyclerView = findViewById(R.id.customRecyclerView);
         adapter = new CustomAdapter(this, photoList);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
-        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
         recyclerView.setAdapter(adapter);
     }
 
--- app/src/main/java/com/example/retrofit/network/GetDataService.java
+++ app/src/main/java/com/example/retrofit/network/GetDataService.java
@@ -7,8 +7,11 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
 
 public interface GetDataService {
+    @Headers({"X-RapidAPI-key: oiweurju08435u092hjfhds98"})
     @GET("/photos")
     Call<List<RetroPhoto>> getAllPhotos();
 
